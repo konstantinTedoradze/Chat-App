8c49093d8abf3512efcff9ad5a8ff8bb914f86e1
konstantinTedoradze <97909996+konstantinTedoradze@users.noreply.github.com>
2022-02-13 11:13:28 +0200

    added connect and disconect

--- server/server.js
+++ server/server.js
@@ -1,12 +1,26 @@
 const path = require('path');
 const express = require('express');
-const app = express();
-const publicPath = path.join(__dirname, '/../public');
+const socketIO = require('socket.io');
+const http = require('http');
 
+const publicPath = path.join(__dirname, '/../public');
 const port = process.env.PORT || 3000;
+const app = express();
+
+let server = http.createServer(app);
+let io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(3000, () => console.log(`server is up on port ${port}`));
+io.on('connection', (socket) => {
+    console.log("A new user just connected");
+    socket.on('disconnect', () => {
+        console.log('User was disconnected');
+    })
+})
+
+
+
+server.listen(3000, () => console.log(`server is up on port ${port}`));
 
 // console.log(path.join(__dirname, '/../public'));
